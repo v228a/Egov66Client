c98eb3fd2458571023a482ed7ba55641037ca273
VoVka <v228a@proton.me>
2025-06-03 00:36:54 +0500

    Замена версий на 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vovka.egov66client"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
